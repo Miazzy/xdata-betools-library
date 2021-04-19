e6d5485aa79ac75154c3c44e7b7e50971c3175e6
yunwisdom <qq695926920@163.com>
2021-04-19 10:50:36 +0800

    init

--- src/object/sealapply.js
+++ src/object/sealapply.js
@@ -136,6 +136,38 @@ const sealapply = {
         },
 
         /**
+         * 查询用印登记的不同状态的列表数据
+         * @param {*} tabname 
+         * @param {*} page 
+         * @param {*} whereSQL 
+         * @param {*} resp 
+         * @returns 
+         */
+        async querySealApplyTabList(tabname, page = 0, whereSQL = '', resp = '') {
+
+            const userinfo = await Betools.storage.getStore('system_userinfo'); //获取当前用户信息
+
+            let { initContractList, sealContractList, failContractList, json_data, json_data_common } = this;
+            let month = dayjs().subtract(12, 'months').format('YYYY-MM-DD'); // 获取最近几个月对应的日期
+            let searchSql = !this.searchWord ? '' : `~and((filename,like,~${this.searchWord}~)~or(serialid,like,~${this.searchWord}~)~or(create_by,like,~${this.searchWord}~)~or(workno,like,~${this.searchWord}~)~or(contract_id,like,~${this.searchWord}~)~or(seal_man,like,~${this.searchWord}~)~or(sign_man,like,~${this.searchWord}~)~or(front_name,like,~${this.searchWord}~)~or(archive_name,like,~${this.searchWord}~)~or(mobile,like,~${this.searchWord}~)~or(deal_depart,like,~${this.searchWord}~)~or(approve_type,like,~${this.searchWord}~))`;
+            let sealTypeSql = (this.sealType === 0 || tabname == '合同类') ? `~and(seal_type,like,合同类)` : ((this.sealType === 1 || tabname == '非合同类') ? `~and(seal_type,like,非合同类)` : '');
+            let status = tabname == 1 ? '待用印' : (tabname == 2 ? '已用印,已领取,移交前台,财务归档,档案归档,已完成' : (tabname == 6 || tabname == 0 ? '已退回' : ''));
+
+            if (tabname == 1 || tabname == 2 || tabname == 6 || tabname == 0) {
+                resp = await Betools.manage.querySealListByConStatus(status, month, userinfo, sealTypeSql, searchSql, page);
+                initContractList = tabname == 1 ? resp.result : initContractList;
+                sealContractList = tabname == 2 ? resp.result : sealContractList;
+                failContractList = (tabname == 6 || tabname == 0) ? resp.result : failContractList;
+            } else if (tabname == '非合同类' || tabname == '合同类') {
+                resp = await Betools.manage.querySealListByConType(userinfo, sealTypeSql, searchSql);
+                json_data_common = tabname == '非合同类' ? resp.result : json_data_common;
+                json_data = tabname == '合同类' ? resp.result : json_data;
+            }
+
+            return { initContractList, sealContractList, failContractList, json_data, json_data_common, resp };
+        },
+
+        /**
          * 跳转到用印合同详情
          * @param {*} item 
          * @param {*} tabname 
