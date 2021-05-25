dc519e8714c2cbb135271fcc8e3c0c85b5b6fada
yunwisdom <qq695926920@163.com>
2021-05-25 10:39:25 +0800

    init

--- src/object/tools.js
+++ src/object/tools.js
@@ -613,6 +613,51 @@ function contain(origin, arg) {
     return ready;
 }
 
+/**
+ * 判断两个字符串相似度
+ * @param {*} s 
+ * @param {*} t 
+ * @param {*} f 
+ * @returns 
+ */
+function similar(s, t, f) {
+    if (!s || !t) {
+        return 0
+    }
+    var l = s.length > t.length ? s.length : t.length
+    var n = s.length
+    var m = t.length
+    var d = []
+    f = f || 3
+    var min = function(a, b, c) {
+        return a < b ? (a < c ? a : c) : (b < c ? b : c)
+    }
+    var i, j, si, tj, cost
+    if (n === 0) return m
+    if (m === 0) return n
+    for (i = 0; i <= n; i++) {
+        d[i] = []
+        d[i][0] = i
+    }
+    for (j = 0; j <= m; j++) {
+        d[0][j] = j
+    }
+    for (i = 1; i <= n; i++) {
+        si = s.charAt(i - 1)
+        for (j = 1; j <= m; j++) {
+            tj = t.charAt(j - 1)
+            if (si === tj) {
+                cost = 0
+            } else {
+                cost = 1
+            }
+            d[i][j] = min(d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + cost)
+        }
+    }
+    let res = (1 - d[n][m] / l)
+    return res.toFixed(f)
+}
+
 var toolsExports = {
     tools: {
         filterObj,
@@ -652,6 +697,7 @@ var toolsExports = {
         contain,
         throttle,
         debounce,
+        similar,
     },
 }
 
